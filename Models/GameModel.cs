259d256220cf0d4acbe19535e927d842a1e5360d
Eric Martinez-Velazquez <auesomeg@gmail.com>
2020-12-29 21:23:24 +0000

    Update namespace name to Sybilline and add Game Model

---
+++ Models/GameModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Sybilline.Models
+{
+    public class GameModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}
